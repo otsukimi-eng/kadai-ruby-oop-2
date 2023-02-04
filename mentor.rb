54e051b743173a5e7111db66842fb86a257814c0
otsukimi <sonnesonnesonne23@gmail.com>
2023-02-04 17:14:41 +0000

    1st

---
+++ mentor.rb
@@ -0,0 +1,26 @@
+class Mentor
+  # インスタンス変数
+  attr_accessor :name
+
+  def initialize(name)
+    self.name = name
+  end
+
+  def job(name)
+    puts "#{self.name}です。私は現役のITプロフェッショナルです。"
+  end  
+end
+
+
+class RailsMentor < Mentor
+ 
+   def job(name)
+    puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
+  end
+end
+
+kirameki = Mentor.new("煌木")
+akaide = RailsMentor.new("赤出")
+
+kirameki.job("煌木")
+akaide.job("赤出")
